a8a3e27ef8ad95090480f13d19c628db66934993
it0m0k <mf22009@shibaura-it.ac.jp>
2022-12-12 18:44:25 +0900

    test02

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App test branch"/>
+  <HelloWorld msg="Welcome to Your Vue.js App merge with test2 branch"/>
 </template>
 
 <script>
